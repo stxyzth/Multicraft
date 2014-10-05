a1727970c652f14e870f8b2e201a8676a01c656b
Stewart Smith <stxyzth@users.noreply.github.com>
2014-10-05 16:26:17 +0100

    Cleaning up

---
+++ themes/bootstrap/views/daemon/index.php
@@ -0,0 +1,124 @@
+<div class="col-md-4">
+<?php
+/**
+ *
+ *   Copyright © 2010-2012 by xhost.ch GmbH
+ *
+ *   All rights reserved.
+ *
+ **/
+ Yii::app()->getComponent("bootstrap");
+$this->pageTitle=Yii::app()->name . ' - '.Yii::t('admin', 'Minecraft Manager Settings');
+$this->widget('booster.widgets.TbBreadcrumbs', array(
+    'links'=>array(Yii::t('admin', 'Settings')=>array('index')),
+    )
+);
+echo CHtml::openTag('div class="well" style="max-width: 330px;"');
+$this->widget('booster.widgets.TbMenu', array('type'=>'list','stacked'=>false,'items'=>array(
+        array(
+            'label'=>Yii::t('admin', 'Update Minecraft'),
+            'url'=>array('daemon/updateMC'),
+            'icon'=>'upload',
+        ),
+        array(
+            'label'=>Yii::t('admin', 'Multicraft Status'),
+            'url'=>array('daemon/status'),
+            'icon'=>'globe',
+        ),
+        array(
+            'label'=>Yii::t('admin', 'Panel Configuration'),
+            'url'=>array('daemon/panelConfig'),
+            'icon'=>'cog',
+        ),
+        array(
+            'label'=>Yii::t('admin', 'Statistics'),
+            'url'=>array('daemon/statistics'),
+            'icon'=>'stats',
+        ),
+        array(
+            'label'=>Yii::t('admin', 'Operations'),
+            'url'=>array('daemon/operations'),
+            'icon'=>'tasks',
+        ),
+        array(
+            'label'=>Yii::t('admin', 'Config File Settings'),
+            'url'=>array('configFile/index'),
+            'icon'=>'file',
+        ),
+      ))
+    );
+echo CHtml::closeTag('div');
+Yii::app()->clientScript->registerCssFile(Theme::css('detailview.css'));
+Yii::app()->getClientScript()->registerCoreScript('jquery');
+
+?>
+</div>
+<div class="col-md-8">
+    <div class="panel panel-default">
+        <div class="table-responsive">
+    <table class="table table-hover">
+<?php echo CHtml::beginForm() ?>
+<?php echo CHtml::hiddenField('submit', 'true') ?>
+<tr class="titlerow">
+    <td><?php echo Yii::t('admin', 'Setting') ?></td>
+    <td></td>
+    <td><?php echo Yii::t('admin', 'Default') ?></td>
+</tr>
+<?php
+echo CHtml::css('.adv { display: none; }');
+$i = 0;
+$adv = false;
+foreach ($settings as $name=>$setting): ?>
+<?php if (@$setting['adv'] && !$adv): ?>
+<tr class="<?php echo ($i++ % 2) ? 'even' : 'odd' ?>"style="height: 32px" >
+    <td><?php echo Theme::img('icons/closed.png', '', array('id'=>'advImg', 'onclick'=>'return checkAdv()')) ?></td>
+    <td><?php echo CHtml::link(Yii::t('admin', 'Show Advanced Options'), '#', array('id'=>'advTxt', 'onclick'=>'return checkAdv()')) ?></td>
+    <td></td>
+</tr>
+<?php $adv = true ?>
+<?php endif ?>
+<tr class="<?php echo ($i++ % 2) ? 'even' : 'odd' ?><?php echo (@$setting['adv'] ? ' adv' : '') ?>">
+    <td><?php echo CHtml::label($setting['label'], 'Setting['.$name.']') ?></td>
+    <?php if ($setting['unit'] == 'bool'): ?>
+    <td><?php echo CHtml::checkBox('Setting['.$name.']', $setting['value']) ?></td>
+    <td><?php echo $setting['default'] ? Yii::t('admin', 'true') : Yii::t('admin', 'false') ?></td>
+    <?php elseif (is_array($setting['unit'])): ?>
+    <td><?php echo CHtml::dropDownList('Setting['.$name.']', $setting['value'], $setting['unit']) ?></td>
+    <td><?php echo $setting['unit'][$setting['default']] ?></td>
+    <?php else: ?>
+    <td><?php echo CHtml::textField('Setting['.$name.']', $setting['value']).' '.$setting['unit'] ?></td>
+    <td><?php echo $setting['default'].' '.$setting['unit'] ?></td>
+    <?php endif ?>
+</tr>
+<?php endforeach ?>
+<tr>
+    <td></td>
+    <td><?php $this->widget('booster.widgets.TbButton',
+    	array('buttonType' => 'submit','label' => 'Save','context' => 'primary','url' => Yii::t('admin', 'Save'))); ?>
+    </td>
+    <td></td>
+</tr>
+</table>
+</div>
+</div>
+<?php
+echo CHtml::script('
+    advShow = false;
+    imgOpen = "'.Theme::themeFile('images/icons/open.png').'";
+    imgClosed = "'.Theme::themeFile('images/icons/closed.png').'";
+    txtOpen = "'.Yii::t('admin', 'Hide Advanced Options').'";
+    txtClosed = "'.Yii::t('admin', 'Show Advanced Options').'";
+    function checkAdv()
+    {
+        advShow = !advShow;
+        $("#advImg").attr("src", advShow ? imgOpen : imgClosed);
+        $("#advTxt").html(advShow ? txtOpen : txtClosed);
+        $(".adv").toggle(advShow);
+        return false;
+    }
+    '.(@$advanced ? '$(function() { checkAdv(); });' : '').'
+');
+echo CHtml::endForm();
+?>
+<br/>
+</div>
---
+++ themes/bootstrap/views/server/index.php
@@ -0,0 +1,118 @@
+<div class="col-md-4">
+<?php
+/**
+ *
+ *   Copyright © 2010-2012 by xhost.ch GmbH
+ *
+ *   All rights reserved.
+ *
+ **/
+ 
+$this->pageTitle = Yii::app()->name . ' - '.Yii::t('mc', 'Server List');
+Yii::app()->getComponent("bootstrap");
+$this->widget('booster.widgets.TbBreadcrumbs', array(
+		'links'=>array(Yii::t('mc', 'Servers'),
+		))
+);
+echo CHtml::css('
+#manage { display: none; }
+');
+echo CHtml::openTag('div class="well" style="max-width: 330px;"');
+if (Yii::app()->user->isSuperuser())
+{
+$this->widget('booster.widgets.TbMenu', array('type'=>'list','stacked'=>false,'items'=>array(
+        array('label'=>Yii::t('mc', 'Create Server'),'url'=>array('create'),'icon'=>'pencil',),
+        '',
+        array('label'=>Yii::t('mc', 'Manage Players'),'url'=>array('player/admin'),'visible'=>Yii::app()->user->isSuperuser(),'icon'=>'user',),
+        array('label'=>Yii::t('mc', 'Manage Commands'),'url'=>array('command/admin'),'visible'=>Yii::app()->user->isSuperuser(),'icon'=>'hdd',),
+    	array('label'=>Yii::t('mc', 'Manage Tasks'),'url'=>array('schedule/admin'),'visible'=>Yii::app()->user->isSuperuser(),'icon'=>'dashboard',),
+        array('label'=>Yii::t('mc', 'Manage Servers'),'url'=>array('server/admin'),'visible'=>Yii::app()->user->isSuperuser(),'icon'=>'tasks',),
+        ),
+      )
+    );
+}
+else
+{
+    if (!$my)
+    {
+        $this->menu[] = array(
+            'label'=>Yii::t('mc', 'My Servers'),
+            'url'=>array('server/index', 'my'=>true),
+            'visible'=>!Yii::app()->user->isGuest,
+            'icon'=>'tasks'
+        );
+    }
+    else
+    {
+        $this->menu[] = array(
+            'label'=>Yii::t('mc', 'All Servers'),
+            'url'=>array('server/index'),
+            'visible'=>!Yii::app()->user->isGuest,
+            'icon'=>'globe'
+        );
+    }
+}
+echo CHtml::closeTag('div');
+?>
+</div>
+<div class="col-md-8">
+<?php
+echo CHtml::script('
+    imgOpen = "'.Theme::themeFile('images/icons/open.png').'";
+    imgClosed = "'.Theme::themeFile('images/icons/closed.png').'";
+    menuShown = {}
+    function showSub(name)
+    {
+        menuShown[name] = !menuShown[name];
+        $("#"+name+"_main").children("img").attr("src", !menuShown[name] ? imgClosed : imgOpen);
+        $("#"+name).stop(true, true).slideToggle(menuShown[name]);
+    }
+');
+if (!!Yii::app()->params['ajax_serverlist'])
+    echo CHtml::script('
+    function get_status(server)
+    {
+        '.CHtml::ajax(array(
+            'type'=>'POST',
+            'dataType'=>'json',
+            'data'=>array(
+                'ajax'=>'get_status',
+                'server'=>'js:server',
+                Yii::app()->request->csrfTokenName=>Yii::app()->request->csrfToken,
+                ),
+            'success'=>'status_response'
+            )).'
+    }
+
+    function status_response(data)
+    {
+        id = parseInt(data["id"]);
+        pl = parseInt(data["pl"]);
+        img = pl >= 0 ? \''.Theme::img('online.png').'\' : \''.Theme::img('offline.png').'\';
+        $("#sv_icon_"+id).html(img);
+        if (pl >= 0)
+        {
+            str = pl;
+            $("#sv_maxplr_"+id).show();
+            $("#sv_chatlink_"+id).show();
+        }
+        else
+        {
+            str = "'.Yii::t('mc', 'Offline').'" + (pl == -2 ? " ('.Yii::t('mc', 'error').')" : "");
+        }
+        $("#sv_status_"+id).html(str);
+    }
+');
+?>
+
+<?php $this->widget('zii.widgets.CListView', array(
+    'emptyText'=>'<br/>'.Yii::t('mc', 'Your own servers and other servers you have access to will be listed here.'),
+    'dataProvider'=>$dataProvider,
+    'itemView'=>'_view',
+    'ajaxUpdate'=>false,
+    'sortableAttributes'=>array(
+        'name'=>Yii::t('mc', 'Name'),
+    ),
+));
+?>
+</div>
